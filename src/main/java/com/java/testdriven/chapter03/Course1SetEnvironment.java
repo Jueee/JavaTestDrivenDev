9bceb2be384eebabe746dcdb10f4853e25ed59d9
尉勇强2 <hzweiyongqiang@corp.netease.com>
2018-05-15 18:47:11 +0800

    “红灯 － 绿灯 － 重构”

---
+++ src/main/java/com/java/testdriven/chapter03/Course1SetEnvironment.java
@@ -0,0 +1,19 @@
+package com.java.testdriven.chapter03;
+
+/**
+ * 	搭建环境
+ *	
+ *	务必将测试和实现代码分开。
+ *	这样做的好处是：可避免不小心将测试与产品二进制文件一起打包；很多构建工具都假定测试位于特定的源代码目录。
+ *
+ *	一种常见的做法是，确保至少有两个源代码目录：
+ *	将实现代码放在目录 src/main/java 中，并将测试代码放在目录 src/test/java 中。
+ *	
+ *	较大的项目中，源代码目录可能更多，但依然必须将实现和测试分开。
+ *	诸如 Maven 和 Gradle 等构建工具要求将源代码目录分开，还要求遵循特定的命名约定。
+ *
+ *	@author hzweiyongqiang
+ */
+public class Course1SetEnvironment {
+
+}
---
+++ src/main/java/com/java/testdriven/chapter03/Course24RunAllTests.java
@@ -0,0 +1,10 @@
+package com.java.testdriven.chapter03;
+
+/**
+ * 	运行所有测试
+ *	
+ *	@author hzweiyongqiang
+ */
+public class Course24RunAllTests {
+
+}
