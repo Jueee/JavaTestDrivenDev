c2317e89486de1a9ad1cea39b3240d7870fd87c9
尉勇强2 <hzweiyongqiang@corp.netease.com>
2018-05-19 19:17:22 +0800

    启动本地jetty服务器

--- src/main/java/com/java/testdriven/chapter08/Course26Preliminary.java
+++ src/main/java/com/java/testdriven/chapter08/Course26Preliminary.java
@@ -19,7 +19,11 @@ package com.java.testdriven.chapter08;
  *	四、查看文件 BooksEndpoint.java：
  *		1、遗留代码的特征之一：两个依赖（ books 和 users ）都是在端点内创建的，而不是注入的。
  *	五、我们可记录重构期间要使用的元素，以便后面编写将依赖注入 BooksEndpoint 的代码。
- *
+ *	六、使用如下命令，在本地执行这个遗留软件：mvn clean jetty:run
+ *		这将启动一个处理请求的本地jetty服务器。
+ *		这样做的最大好处是，部署是自动完成的，无需将一切打包并手动将其部署到应用程序服务器（如JBoss AS、GlassFish、Geronimo或TomEE）。
+ *		这可极大提高修改并查看效果的速度，从而缩短反馈时间。
+ *		在本章后面，我们将在Java代码中以编程方式启动这个服务器。
  *
  *	@author hzweiyongqiang
  */
